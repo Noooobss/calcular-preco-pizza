5e0bf4814a2e6068ae61e082396f47aed5df50e8
Noooobss <joaobrawstar20@gmail.com>
2024-10-30 21:24:30 -0300

    app.py

--- app.py
+++ app.py
@@ -2,23 +2,21 @@ import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+#Analisa o algoritimo
 df = pd.read_csv("pizzas.csv")
 df.plot(kind="scatter", x="diametro", y="preco");
 modelo = LinearRegression()
 x = df[["diametro"]]
 y = df[["preco"]]
-
 modelo.fit(x ,y)
 
+#Configuração do site
 st.title("Prever valor da Pizza - Pizzaria Cérebro Pensante")
 st.write("Site teste de Machine Learning feito Por: João Pedro Kapp - Noooob")
 st.write("Site desenvolvido em: Python e Jupyter Notebook")
 st.divider()
 diametro = st.number_input("Digite o tamanho do diametro do pizza em cm: ")
-
 if diametro:
     preco_previsto = modelo.predict([[diametro]])[0][0]
     st.write(f"O valor da Pizza com diametro de {diametro}cm, custa um total de: R${preco_previsto:.2f}")
     st.write("Na compra de três pizzas ganha uma Pizza doce de brinde!")
-    st.divider()
-    st.write("")
