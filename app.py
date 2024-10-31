5ee7df046bdd3eb071c808ce785ac0fbcb6360e8
Noooobss <joaobrawstar20@gmail.com>
2024-10-30 22:26:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ st.write("Site teste de Machine Learning feito Por: João Pedro Kapp - Noooob")
 st.write("Site desenvolvido em: Python e Jupyter Notebook")
 st.divider()
 diametro = st.number_input("Digite o tamanho do diametro do pizza em cm: ")
-if diametro:
+if diametro > 0:
     preco_previsto = modelo.predict([[diametro]])[0][0]
     st.write(f"O valor da Pizza com diametro de {diametro}cm, custa um total de: R${preco_previsto:.2f}")
     st.write("Na compra de três pizzas ganha uma Pizza doce de brinde!")
     st.divider()
+else:
+    st.write("A Pizza não pode ter um diametrro negativo!")
